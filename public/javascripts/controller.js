70fe0abaac24fae3566ab9653cbe8308f8dd4098
catiecook <catiecook888@gmail.com>
2017-01-23 21:33:05 -0700

    moving to branch

--- public/javascripts/controller.js
+++ public/javascripts/controller.js
@@ -12,8 +12,6 @@ angular.module('airdna')
     $scope.master.searched = false;
     $scope.master.hideForm = true;
 
-
-
     $scope.search = (bed, bath, accomidates, address, zip) => {
       let addressInfo = {
         address: address,
@@ -25,9 +23,6 @@ angular.module('airdna')
 
       $scope.master.push(addressInfo);
 
-      $scope.master.searched = true;
-      $scope.master.hideForm = false;
-
       const payload = {
         address: address,
         zip: zip,
@@ -35,17 +30,52 @@ angular.module('airdna')
         bath: bath,
         accomidates: accomidates
       }
-
       $.post('/search', payload)
         .done((data) => {
-          console.log(data.adr_predicted["2016"]);
-          // let predictions = {
-          //   adr: {
-          //     1: data.adr_predicted.2016.1
-          //   }
-          // }
+          console.log(data);
+          let adr = data.adr_predicted["2016"];
+          let monthlyAverageAdr = data.adr_predicted.ltm;
+          let occupancy = data.occ_predicted["2016"];
+          let avOcc = (data.occ_predicted.ltm) * 100;
+          let details = data.property_details;
+          let revenue = data.revenue_potential["2016"];
+          let yearRev = data.revenue_potential.ltm;
+
+          $scope.master.rentalizerPredictions.push({
+            adr: adr,
+            avAdr: monthlyAverageAdr,
+            occupancy: occupancy,
+            avOcc: avOcc,
+            details: details,
+            revenue: revenue,
+            yearRev: yearRev
+          });
+
+          console.log($scope.master.rentalizerPredictions[0]);
+
+          hideForm();
+          showResults();
+          $scope.$apply();
+
         }).fail((err) => {
           console.log("error", err);
         });
     };
+
+    function hideForm() {
+      $scope.master.hideForm = false;
+    };
+
+    function showResults() {
+      $scope.master.searched = true;
+    };
+
+    function newSearch() {
+      console.log("Here");
+      $scope.master.rentalizerPredictions = [];
+      $scope.master.hideForm = true;
+      $scope.master.searched = false;
+      $scope.$apply();
+    }
+
 });
