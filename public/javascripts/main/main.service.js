58db18f1b1d4d1272dabc15597f60ad64343f11f
catiecook <catiecook888@gmail.com>
2017-01-25 12:14:32 -0700

    addresses not wrking, server errors

--- public/javascripts/main/main.service.js
+++ public/javascripts/main/main.service.js
@@ -1,6 +1,8 @@
 angular.module('airdna')
   .service('mainService', function($http) {
     let revPotential = [];
+    let airBnbData = [];
+    var addresses = [];
 
     function getInfo(bed, bath, accomidates, address, zip){
       const payload = {
@@ -21,17 +23,24 @@ angular.module('airdna')
           let details = data.property_details;
           let revenue = data.revenue_potential["2016"];
           let yearRev = data.revenue_potential.ltm;
-          let comp = data.comps;
+          let comp = [];
           let locations = [];
+          let addresses = [];
+
+          comp.push(data.comps);
 
           for(var i=0; i<data.comps.length; i++) {
             locations.push({
               lat: data.comps[i].lat,
-              lon: data.comps[i].lon
+              lon: data.comps[i].lng
             });
           };
 
-          revPotential = revPot(revenue)
+          revPotential = revPot(revenue);
+          airBnbData.push(comp);
+
+          getLatLng(airBnbData, comp[0]);
+          console.log("comp", comp[0]);
 
           return {
             adr: adr,
@@ -66,6 +75,27 @@ angular.module('airdna')
         12: data["12"],
       });
     };
+
+    function getLatLng(data, comp){
+      console.log("LAT LONG");
+      for(var i =0; i<data[0].length; i++) {
+        let lat = data[0][i].lat;
+        let lng = data[0][i].lng;
+        reverseGeocode(lat, lng, comp[i], i);
+      }
+    };
+
+    function reverseGeocode(lat, lng, comp, i) {
+      console.log("in geocode");
+      var url = "https://maps.googleapis.com/maps/api/geocode/json?latlng= " + lat+"," + lng + "&key=AIzaSyB3_vklrmidqHqKCkQWiouLCyQ_I_KTYtE";
+      $http.get(url).then((data) => {
+        // var data = data.data.results
+        console.log(data);
+        // comp.push({
+        //   address: data[i].formatted_address
+        // })
+      })
+    }
 
     return {
       getInfo: getInfo,
